a7a693e022234be2f13d7da11ea6430e09d746dc
HunterR <gonidzze@gmail.com>
2022-04-13 19:45:16 +0300

    Editedg logics of games

--- games/brain-quest.js
+++ games/brain-quest.js
@@ -1,6 +1,15 @@
-import gameStart from '../src/index.js';
+import { gameStart, ranNumb } from '../src/index.js';
+
+export const ishDan = () => {
+  const num = ranNumb(50) + 1;
+  const rightAnsw = (num % 2 === 1) ? 'no' : 'yes';
+  const gameStr = num;
+  return [rightAnsw, gameStr];
+};
 
 const brainQuest = () => {
-  gameStart('even');
+  const privet = 'What is the result of the expression?';
+  gameStart(privet, ishDan);
 };
+
 export default brainQuest;
--- src/index.js
+++ src/index.js
@@ -1,52 +1,15 @@
 import readlineSync from 'readline-sync';
 
-const ranNumb = (velicina) => Math.floor(Math.random() * velicina);
+export const ranNumb = (velicina) => Math.floor(Math.random() * velicina);
 
-const gameStart = (gameKind) => {
+export const gameStart = (privet, ishDan) => {
   console.log('Welcome to the Brain Games!');
   const nam = readlineSync.question('May I have your name? ');
   console.log(`Hi ${nam}!`);
-  let firstLoop = true;
+  console.log(privet);
   let itog = true;
   for (let i = 0; i < 3; i += 1) {
-    let gameStr = '';
-    let rightAnsw;
-    switch (gameKind) {
-      case 'calc': {
-        if (firstLoop === true) {
-          console.log('What is the result of the expression?');
-        }
-        const znak = ['-', '+', '*'];
-        const num1 = ranNumb(50);
-        const num2 = ranNumb(50);
-        const operInd = Math.floor(Math.random() * 3);
-        const oper = znak[operInd];
-        gameStr = num1 + oper + num2;
-        switch (operInd) {
-          case 0:
-            rightAnsw = num1 - num2;
-            break;
-          case 1:
-            rightAnsw = num1 + num2;
-            break;
-          default:
-            rightAnsw = num1 * num2;
-            break;
-        }
-        break;
-      }
-      case 'even': {
-        if (firstLoop === true) {
-          console.log('Answer "yes" if the number is even, otherwise answer "no".');
-        }
-        const num = ranNumb(50) + 1;
-        rightAnsw = (num % 2 === 1) ? 'no' : 'yes';
-        gameStr = num;
-        break;
-      }
-      default:
-        break;
-    }
+    const [rightAnsw, gameStr] = ishDan();
     console.log('Question: ', gameStr);
     const answr = readlineSync.question('Your answer: ');
     if (answr === String(rightAnsw)) {
@@ -57,11 +20,8 @@ const gameStart = (gameKind) => {
       console.log(`Let's try again, ${nam}!`);
       break;
     }
-    firstLoop = false;
   }
   if (itog === true) {
     console.log(`Congratulations, ${nam}!`);
   }
 };
-
-export default gameStart;
