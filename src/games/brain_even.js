e454064ccc3c2295a262070cde629523ab12bdde
HunterR <gonidzze@gmail.com>
2022-04-20 19:04:50 +0300

    Edit utils.js brain-even.js

--- src/games/brain_even.js
+++ src/games/brain_even.js
@@ -1,11 +1,11 @@
 import startGame from '../index.js';
 import { getRandomNumber } from '../utils.js';
 
-const isNumberEven = (num) => (num % 2 === 1);
+const isNumberEven = (num) => (num % 2 === 0);
 
 const initialData = () => {
   const num = getRandomNumber(1, 100);
-  const rightAnswer = (isNumberEven(num)) ? 'no' : 'yes';
+  const rightAnswer = (isNumberEven(num)) ? 'yes' : 'no';
   const gameExpression = String(num);
   return [rightAnswer, gameExpression];
 };
--- src/utils.js
+++ src/utils.js
@@ -1,30 +1,7 @@
 /* eslint-disable import/prefer-default-export */
 
-export const getRandomNumber = (minOfRange, maxOfRange) => {
-  const num1 = Math.ceil(minOfRange);
-  const num2 = Math.floor(maxOfRange);
+export const getRandomNumber = (minRange, maxRange) => {
+  const num1 = Math.ceil(minRange);
+  const num2 = Math.floor(maxRange);
   return Math.floor(Math.random() * (num2 - num1)) + num1;
 };
-
-export const checkPrime = (num) => {
-  const highRange = Math.sqrt(num);
-  for (let i = 2; i <= highRange; i += 1) {
-    if (num % i === 0) {
-      return false;
-    }
-  }
-  return num > 1;
-};
-
-export const highestDivider = (num1, num2) => {
-  let number1 = num1;
-  let number2 = num2;
-  while (number1 !== number2) {
-    if (number1 > number2) {
-      number1 -= number2;
-    } else number2 -= number1;
-  }
-  return number1;
-};
-
-export const isNumberEven = (num) => (num % 2 === 1);
